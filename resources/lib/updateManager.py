bc4bc5abe447a8d05e50c7bd4aff8b8418a3834d
anis3 <anis3.n@web.de>
2019-08-13 08:45:12 +0000

    Update updateManager.py

--- resources/lib/updateManager.py
+++ resources/lib/updateManager.py
@@ -22,7 +22,7 @@ XSTREAM_DIRNAME = os.path.basename(ROOT_DIR)
 
 ## URLRESOLVER
 REMOTE_URLRESOLVER_COMMITS = "https://api.github.com/repos/streamxstream/script.module.urlresolver/commits/master"
-REMOTE_URLRESOLVER_DOWNLOADS = "https://github.com/streamxstream/script.module.urlresolver/archive/master.zip
+REMOTE_URLRESOLVER_DOWNLOADS = "https://github.com/streamxstream/script.module.urlresolver/archive/master.zip"
 ## XSTREAM
 REMOTE_XSTREAM_COMMITS = "https://api.github.com/repos/streamxstream/plugin.video.xstream/commits/nightly"
 REMOTE_XSTREAM_NIGHTLY = "https://github.com/streamxstream/plugin.video.xstream/archive/nightly.zip"
