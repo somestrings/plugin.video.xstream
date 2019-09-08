e4bacd692efe54fde183b57a07e03174101d5c8a
streamxstream <53152297+streamxstream@users.noreply.github.com>
2019-09-08 16:48:03 +0200

    Undo update

--- resources/lib/updateManager.py
+++ resources/lib/updateManager.py
@@ -21,8 +21,8 @@ XSTREAM_DIRNAME = os.path.basename(ROOT_DIR)
 
 
 ## URLRESOLVER
-REMOTE_URLRESOLVER_COMMITS = "https://api.github.com/repos/streamxstream/script.module.urlresolver/commits/master?access_token=5de8fcae5f8e27e444abd0f53a56521510302d97"
-REMOTE_URLRESOLVER_DOWNLOADS = "https://api.github.com/repos/streamxstream/script.module.urlresolver/zipball/master?access_token=5de8fcae5f8e27e444abd0f53a56521510302d97"
+REMOTE_URLRESOLVER_COMMITS = "https://api.github.com/repos/streamxstream/script.module.urlresolver/commits/master"
+REMOTE_URLRESOLVER_DOWNLOADS = "https://api.github.com/repos/streamxstream/script.module.urlresolver/archive/master.zip"
 
 ## XSTREAM
 REMOTE_XSTREAM_COMMITS = "https://api.github.com/repos/streamxstream/plugin.video.xstream/commits/nightly"
