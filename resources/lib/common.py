595425964d12999d511205fa1ec9af6e9598dea6
anis3 <anis3.n@web.de>
2020-01-08 22:06:21 +0000

    Code cleanup

--- resources/lib/common.py
+++ resources/lib/common.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import xbmcaddon
 from xbmc import translatePath
 
@@ -12,5 +13,4 @@ FF_USER_AGENT = 'Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36
 OPERA_USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36 OPR/34.0.2036.50'
 IOS_USER_AGENT = 'Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25'
 ANDROID_USER_AGENT = 'Mozilla/5.0 (Linux; Android 4.4.2; Nexus 4 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.114 Mobile Safari/537.36'
-
 UA_LIST = [IE_USER_AGENT, FF_USER_AGENT, OPERA_USER_AGENT, IOS_USER_AGENT, ANDROID_USER_AGENT]
