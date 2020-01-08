f9b60eab13f71b8bc635af855f18aeb4eb9cd4a5
anis3 <anis3.n@web.de>
2020-01-08 22:14:05 +0000

    Code cleanup

--- resources/lib/logger.py
+++ resources/lib/logger.py
@@ -22,20 +22,15 @@ def fatal(sInfo):
 
 def __writeLog(sLog, cLogLevel=xbmc.LOGDEBUG):
     params = ParameterHandler()
-
     try:
         if isinstance(sLog, unicode):
             sLog = '%s (ENCODED)' % (sLog.encode('utf-8'))
-
         if params.exist('site'):
             site = params.getValue('site')
             sLog = "\t[%s] -> %s: %s" % (common.addonName, site, sLog)
         else:
             sLog = "\t[%s] %s" % (common.addonName, sLog)
-
         xbmc.log(sLog, cLogLevel)
-
-
     except Exception as e:
         try:
             xbmc.log('Logging Failure: %s' % (e), cLogLevel)
