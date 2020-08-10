858e42738faadab7996aeea2e605770389caa398
anis3 <anis3.n@web.de>
2020-08-10 21:45:57 +0000

    py23

--- service.py
+++ service.py
@@ -1,13 +1,11 @@
 # -*- coding: UTF-8 -*-
+import sys, os, xbmc, xbmcaddon, xbmcgui
 
-import os
-import xbmc
-import xbmcaddon
-import xbmcgui
-
-AddonID = xbmcaddon.Addon().getAddonInfo('id')
 AddonName = xbmcaddon.Addon().getAddonInfo('name')
-NIGHTLY_VERSION_CONTROL = os.path.join(xbmc.translatePath(xbmcaddon.Addon().getAddonInfo('profile')).decode('utf-8'), "update_sha")
+if sys.version_info[0] == 2:
+    NIGHTLY_VERSION_CONTROL = os.path.join(xbmc.translatePath(xbmcaddon.Addon().getAddonInfo('profile')).decode('utf-8'), "update_sha")
+else:
+    NIGHTLY_VERSION_CONTROL = os.path.join(xbmc.translatePath(xbmcaddon.Addon().getAddonInfo('profile')), "update_sha")
 
 def infoDialog(message, heading=AddonName, icon='', time=5000, sound=False):
     if icon == '': icon = xbmcaddon.Addon().getAddonInfo('icon')
@@ -36,5 +34,4 @@ except:
 #if not 'false' in xbmc.executeJSONRPC('{"jsonrpc":"2.0", "id":1, "method":"Settings.GetSettingValue", "params":{"setting":"filelists.showparentdiritems"}}'):
 #    xbmc.executeJSONRPC('{"jsonrpc":"2.0","method":"Settings.SetSettingValue","id":1,"params":{"setting":"filelists.showparentdiritems","value":false}}')
 #    xbmc.executebuiltin("ReloadSkin()")
-
-exit(0)
+#exit(0)
