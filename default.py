09d99d95f0245e58a5c20921c37f2877650d08d8
anis3 <anis3.n@protonmail.com>
2021-02-13 19:28:02 +0000

    Update default.py

--- default.py
+++ default.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from xstream import run
 from os.path import join
 from sys import path
 import platform
@@ -6,13 +7,11 @@ from resources.lib import common
 from resources.lib.tools import logger
 
 _addonPath_ = common.addonPath
-path.append(join(_addonPath_, "resources", "lib"))
-path.append(join(_addonPath_, "resources", "lib", "gui"))
-path.append(join(_addonPath_, "resources", "lib", "handler"))
-path.append(join(_addonPath_, "resources", "art", "sites"))
-path.append(join(_addonPath_, "sites"))
-
-from xstream import run
+path.append(join(_addonPath_, 'resources', 'lib'))
+path.append(join(_addonPath_, 'resources', 'lib', 'gui'))
+path.append(join(_addonPath_, 'resources', 'lib', 'handler'))
+path.append(join(_addonPath_, 'resources', 'art', 'sites'))
+path.append(join(_addonPath_, 'sites'))
 logger.info('*---- Running xStream, version %s ----*' % common.addon.getAddonInfo('version'))
 logger.info('Python-Version: %s' % platform.python_version())
 
@@ -20,10 +19,10 @@ try:
     run()
 except Exception as e:
     if str(e) == 'UserAborted':
-        logger.error("User aborted list creation")
+        logger.error('User aborted list creation')
     else:
         import traceback
         import xbmcgui
         logger.error(traceback.format_exc())
-        value = (str(e.__class__.__name__) + " : " + str(e), str(traceback.format_exc().splitlines()[-3].split('addons')[-1]))
+        value = (str(e.__class__.__name__) + ' : ' + str(e), str(traceback.format_exc().splitlines()[-3].split('addons')[-1]))
         dialog = xbmcgui.Dialog().ok('Error', str(value))
