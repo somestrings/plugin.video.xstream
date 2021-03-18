b35457478da2a79768e706a4a89cc6c1b5c829c9
anis3 <anis3.n@protonmail.com>
2021-03-18 15:51:56 +0000

    code cleanup

--- resources/lib/handler/myjdownloaderHandler.py
+++ resources/lib/handler/myjdownloaderHandler.py
@@ -1,47 +1,42 @@
 from resources.lib.config import cConfig
 from resources.lib.gui.gui import cGui
-from resources.lib.handler.requestHandler import cRequestHandler
 from resources.lib.tools import logger
 import myjdapi
+
 
 class cMyJDownloaderHandler:
 
     def sendToMyJDownloader(self, sUrl, sMovieTitle):
-        if (self.__checkConfig() == False):
+        if self.__checkConfig() == False:
             cGui().showError('MyJDownloader', 'Settings ueberpruefen', 5)
             return False
 
         jd = myjdapi.Myjdapi()
-        if (jd.connect(self.__getUser(), self.__getPass()) == False):
+        if jd.connect(self.__getUser(), self.__getPass()) == False:
             cGui().showError('MyJDownloader', 'Keine Verbindung zu MY.JDownloader', 5)
             return False
 
-        if (jd.update_devices() == False):
+        if jd.update_devices() == False:
             cGui().showError('MyJDownloader', 'Konnte Geraete Liste nicht laden', 5)
             return False
 
         device = jd.get_device(self.__getDevice())
-        if (device.linkgrabber.add_links([{"autostart" : False, "links" : sUrl, "packageName" : sMovieTitle }])['id'] > 0):
+        if device.linkgrabber.add_links([{"autostart": False, "links": sUrl, "packageName": sMovieTitle}])['id'] > 0:
             cGui().showInfo('MyJDownloader', 'Link gesendet', 5)
             return True
         return False
 
     def __checkConfig(self):
         logger.info('check MYJD Addon setings')
-        oConfig = cConfig()
-        bEnabled = oConfig.getSetting('myjd_enabled')
-        if (bEnabled == 'true'):
+        if cConfig().getSetting('myjd_enabled') == 'true':
             return True
         return False
 
     def __getDevice(self):
-        oConfig = cConfig()
-        return oConfig.getSetting('myjd_device')
+        return cConfig().getSetting('myjd_device')
 
     def __getUser(self):
-        oConfig = cConfig()
-        return oConfig.getSetting('myjd_user')
+        return cConfig().getSetting('myjd_user')
 
     def __getPass(self):
-        oConfig = cConfig()
-        return oConfig.getSetting('myjd_pass')
+        return cConfig().getSetting('myjd_pass')
