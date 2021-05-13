dd17080f56092a189fb69e8ef11008de87808c2d
anis3 <20500872+anis3@users.noreply.github.com>
2021-05-13 23:01:13 +0000

    Update kkiste.py

--- sites/kkiste.py
+++ sites/kkiste.py
@@ -9,7 +9,7 @@ from resources.lib.gui.gui import cGui
 SITE_IDENTIFIER = 'kkiste'
 SITE_NAME = 'KKiste'
 SITE_ICON = 'kkiste.png'
-URL_MAIN = cConfig().getSetting('kkisteurl', 'https://kkiste.co')
+URL_MAIN = 'https://kkiste.movie/'
 
 
 def load():
