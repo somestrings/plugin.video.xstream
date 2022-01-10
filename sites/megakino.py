5759121e4a75da124bd71a333b8b148bc0d4dbff
anis3 <anis3@protonmail.com>
2022-01-10 11:26:29 +0100

    add megakino - thx betegeuse46

---
+++ sites/megakino.py
@@ -0,0 +1,169 @@
+# -*- coding: utf-8 -*-
+from resources.lib.handler.ParameterHandler import ParameterHandler
+from resources.lib.handler.requestHandler import cRequestHandler
+from resources.lib.tools import logger, cParser
+from resources.lib.gui.guiElement import cGuiElement
+from resources.lib.gui.gui import cGui
+
+SITE_IDENTIFIER = 'megakino'
+SITE_NAME = 'Megakino'
+SITE_ICON = 'megakino.png'
+URL_MAIN = 'https://megakino.co/'
+URL_KINO = URL_MAIN + 'kinofilme/'
+URL_FILME = URL_MAIN + 'films/'
+URL_SERIEN = URL_MAIN + 'serials/'
+URL_ANIMATION = URL_MAIN + 'multfilm/'
+SITE_GLOBAL_SEARCH = False
+
+
+def load():
+    logger.info('Load %s' % SITE_NAME)
+    params = ParameterHandler()
+    params.setParam('sUrl', URL_MAIN)
+    cGui().addFolder(cGuiElement('Neues', SITE_IDENTIFIER, 'showEntries'), params)
+    params.setParam('sUrl', URL_KINO)
+    cGui().addFolder(cGuiElement('Kinofilme', SITE_IDENTIFIER, 'showEntries'), params)
+    params.setParam('sUrl', URL_FILME)
+    cGui().addFolder(cGuiElement('Filme', SITE_IDENTIFIER, 'showEntries'), params)
+    params.setParam('sUrl', URL_SERIEN)
+    cGui().addFolder(cGuiElement('Serien', SITE_IDENTIFIER, 'showEntries'), params)
+    params.setParam('sUrl', URL_ANIMATION)
+    cGui().addFolder(cGuiElement('Animation', SITE_IDENTIFIER, 'showEntries'), params)
+    params.setParam('sUrl', URL_MAIN)
+    cGui().addFolder(cGuiElement('Genre', SITE_IDENTIFIER, 'showGenre'), params)
+    cGui().setEndOfDirectory()
+
+
+def showGenre():
+    params = ParameterHandler()
+    entryUrl = params.getValue('sUrl')
+    sHtmlContent = cRequestHandler(entryUrl).request()
+    pattern = '<div class="side-block__title">Genres</div>(.*?)</ul>\s*</div>'
+    isMatch, sHtmlContainer = cParser.parseSingleResult(sHtmlContent, pattern)
+    if isMatch:
+        pattern = 'href="([^"]+)">([^<]+)</a>'
+        isMatch, aResult = cParser.parse(sHtmlContainer, pattern)
+    if not isMatch:
+        cGui().showInfo()
+        return
+
+    for sUrl, sName in aResult:
+        params.setParam('sUrl', sUrl)
+        cGui().addFolder(cGuiElement(sName, SITE_IDENTIFIER, 'showEntries'), params)
+    cGui().setEndOfDirectory()
+
+
+def showEntries(entryUrl=False, sGui=False, sSearchText=False):
+    oGui = sGui if sGui else cGui()
+    params = ParameterHandler()
+    if not entryUrl: entryUrl = params.getValue('sUrl')
+    oRequest = cRequestHandler(entryUrl, ignoreErrors=(sGui is not False))
+    sHtmlContent = oRequest.request()
+    pattern = '<a class=[^>]*href="([^"]+)">\s*<div class="[^"]+">\s*<img data-src="([^"]+)" src="[^"]+" alt="([^"]+)">\s*<div class="poster__label">(.*?</div>)\s*</div>\s*</a>'
+    isMatch, aResult = cParser.parse(sHtmlContent, pattern)
+
+    if not isMatch:
+        if not sGui: oGui.showInfo()
+        return
+
+    total = len(aResult)
+    for sUrl, sThumbnail, sName, sInfo in aResult:
+        isTvshow, aResult = cParser.parse(sName, ' - Staffel \d+')
+        oGuiElement = cGuiElement(sName, SITE_IDENTIFIER, 'showEpisodes' if isTvshow else 'showHosters')
+        isDesc, sDesc = cParser.parseSingleResult(sInfo, '<div class="poster__text[^"]+">([^<]+)</div>')
+        if isDesc:
+            if sDesc[-1] != '.':
+                sDesc += '...'
+            oGuiElement.setDescription(sDesc)
+        oGuiElement.setThumbnail(URL_MAIN + sThumbnail)
+        oGuiElement.setMediaType('tvshow' if isTvshow else 'movie')
+        params.setParam('entryUrl', sUrl)
+        params.setParam('sName', sName)
+        params.setParam('sThumbnail', sThumbnail)
+
+        oGui.addFolder(oGuiElement, params, isTvshow, total)
+
+    isMatchNextPage, sNextUrl = cParser().parseSingleResult(sHtmlContent, '">\s*<a href="([^"]+)">\D')
+    if isMatchNextPage:
+        params.setParam('sUrl', sNextUrl)
+        oGui.addNextPage(SITE_IDENTIFIER, 'showEntries', params)
+    oGui.setView('tvshows' if isTvshow else 'movies')
+    oGui.setEndOfDirectory()
+
+
+def showEpisodes():
+    params = ParameterHandler()
+    sUrl = params.getValue('entryUrl')
+    sThumbnail = params.getValue("sThumbnail")
+    sName = params.getValue('sName')
+    isMatch, sShowName = cParser.parseSingleResult(sName, '(.*?) - Staffel \d+')
+    if not isMatch:
+        if not sGui: oGui.showInfo()
+        return
+    isMatch, sSeason = cParser.parseSingleResult(sName, ' - Staffel (\d+)')
+    if not isMatch:
+        if not sGui: oGui.showInfo()
+        return
+
+    sHtmlContent = cRequestHandler(sUrl).request()
+    pattern = '<option value="ep([^"]+)">([^<]+)</option>'
+    isMatch, aResult = cParser.parse(sHtmlContent, pattern)
+    total = len(aResult)
+    for episode, episodeName in aResult:
+        params.setParam('episodeId', episode)
+        oGuiElement = cGuiElement(str(episodeName), SITE_IDENTIFIER, 'showEpisodeHosters')
+        oGuiElement.setThumbnail(URL_MAIN + sThumbnail)
+        oGuiElement.setEpisode(episode)
+        oGuiElement.setMediaType('episode')
+        cGui().addFolder(oGuiElement, params, False, total)
+    cGui().setView('episodes')
+    cGui().setEndOfDirectory()
+
+
+def showHosters():
+    hosters = []
+    sUrl = ParameterHandler().getValue('entryUrl')
+    sHtmlContent = cRequestHandler(sUrl).request()
+    pattern = '<iframe id="film_main" data-src="([^"]+)"'
+    isMatch, aResult = cParser.parse(sHtmlContent, pattern)
+    if isMatch:
+        for sUrl in aResult:
+            hoster = {'link': sUrl, 'name': cParser.urlparse(sUrl)}
+            hosters.append(hoster)
+    if hosters:
+        hosters.append('getHosterUrl')
+    return hosters
+
+
+def showEpisodeHosters():
+    hosters = []
+    sUrl = ParameterHandler().getValue('entryUrl')
+    episodeId = 'ep' + ParameterHandler().getValue('episodeId')
+    sHtmlContent = cRequestHandler(sUrl).request()
+    pattern = '<select name="pmovie__select-items" class="[^"]+" style="[^"]+" id="%s">\s*(.*?)\s*</select>' % episodeId
+    isMatch, sContainer = cParser.parseSingleResult(sHtmlContent, pattern)
+    if isMatch:
+        pattern = '<option value="([^"]+)">'
+        isMatch, aResult = cParser.parse(sContainer, pattern)
+        if isMatch:
+            for sUrl in aResult:
+                hoster = {'link': sUrl, 'name': cParser.urlparse(sUrl)}
+                hosters.append(hoster)
+    if hosters:
+        hosters.append('getHosterUrl')
+    return hosters
+
+
+def getHosterUrl(sUrl=False):
+    return [{'streamUrl': sUrl, 'resolved': False}]
+
+
+def showSearch():
+    sSearchText = cGui().showKeyBoard()
+    if not sSearchText: return
+    _search(False, sSearchText)
+    cGui().setEndOfDirectory()
+
+
+def _search(oGui, sSearchText):
+    showEntries(URL_SEARCH % cParser.quotePlus(sSearchText), oGui, sSearchText)
